a9c48a51750284564ab332d758ea4538066d7e96
KimEunA97 <kimeuna9712@gmail.com>
2025-10-25 20:29:54 +0900

    nextjs CRA@latest 로 다시 셋업

--- pokedex/src/components/nav/TopNavBar.js
+++
@@ -1,8 +0,0 @@
-export default function TopNavBar() {
-  return (
-    <div className="w-full bg-blue-500 text-white p-4">
-      TopNavBar
-      <h1 className="text-4xl font-bold text-blue-600">Pokedex</h1>
-    </div>
-  );
-}
