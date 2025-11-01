60ad16223d2bdb6ecff63d1a79ae825bbfc7ac6a
KimEunA97 <kimeuna9712@gmail.com>
2025-11-01 23:38:43 +0900

    한글이름 요청 완료

--- pokedex-nextjs/app/(pages)/pokemon/page.tsx
+++ pokedex-nextjs/app/(pages)/pokemon/page.tsx
@@ -1,14 +1,12 @@
 import PokemonCard from "@/app/_components/card/pokemonCard";
+import RandomPokemonCard from "@/app/_components/card/RandomPokemonCard";
 
 export default function PokemonPage() {
   return (
-    <div className="flex items-center justify-center">
-      <div className="grid grid-cols-3  rounded-lg w-11/12  bg-white text-black">
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
+    <div className="flex flex-col items-center justify-center">
+      {/* 포켓몬 랜덤 카드 */}
+      <RandomPokemonCard />
+      <div className="grid grid-cols-3  rounded-lg w-full bg-white text-black">
         <PokemonCard />
       </div>
     </div>
