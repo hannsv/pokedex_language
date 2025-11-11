b882fcc4eb70dd6c7e719d253234dfa7058aa57a
KimEunA97 <kimeuna9712@gmail.com>
2025-11-11 23:20:07 +0900

    [FE] api :: 이름 한글화 함수 내 파라미터 타입 설정

--- pokedex-nextjs/app/lib/api/pokemon-to-language.ts
+++ pokedex-nextjs/app/lib/api/pokemon-to-language.ts
@@ -1,6 +1,8 @@
 export const getPokemonKoreanName = async (id: number) => {
   const resp = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}/`);
   const data = await resp.json();
-  const koreanNameItem = data.names.find((n: any) => n.language.name === "ko");
+  const koreanNameItem = data.names.find(
+    (n: { language: { name: string } }) => n.language.name === "ko"
+  );
   return koreanNameItem ? koreanNameItem.name : data.name; // 한글이 없으면 영문 이름 fallback
 };
