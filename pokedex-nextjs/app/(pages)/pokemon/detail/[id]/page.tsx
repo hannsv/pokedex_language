caa673698a64ff49c816eb282c1198cbd7b6d1f8
KimEunA97 <kimeuna9712@gmail.com>
2025-11-24 21:54:31 +0900

    netlify 배포 테스트

--- pokedex-nextjs/app/(pages)/pokemon/detail/[id]/page.tsx
+++ pokedex-nextjs/app/(pages)/pokemon/detail/[id]/page.tsx
@@ -11,14 +11,14 @@ import React, { useEffect, useState } from "react";
 import pokemonDataRaw from "@/data/pokemon.json";
 
 // 정적 파라미터 생성 (빌드 시 실행)
-export async function generateStaticParams() {
-  return (pokemonDataRaw as { url: string }[]).map((pokemon) => {
-    const id = pokemon.url.split("/").filter(Boolean).pop();
-    return {
-      id: id,
-    };
-  });
-}
+// export async function generateStaticParams() {
+//   return (pokemonDataRaw as { url: string }[]).map((pokemon) => {
+//     const id = pokemon.url.split("/").filter(Boolean).pop();
+//     return {
+//       id: id,
+//     };
+//   });
+// }
 
 // 포켓몬 상세페이지 컴포넌트
 export default function PokemonDetailPage({
